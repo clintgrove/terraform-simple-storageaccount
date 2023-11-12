91b6a65a61ce22ab6b85618d3ed76447e7737ef2
Clint Grove <clintelyonson@gmail.com>
2023-11-12 22:12:22 +0000

    REQ PR

--- Infrastructure/TerraformSimple/main.tf
+++ Infrastructure/TerraformSimple/main.tf
@@ -11,15 +11,6 @@ data "azurerm_client_config" "current" {
 
 }
 
-module "avm-res-keyvault-vault" {
-  source  = "Azure/avm-res-keyvault-vault/azurerm"
-  version = "0.5.0"
-  name = "keyvaultcgr2terraf"
-  resource_group_name   = var.resourcegroup
-  location              = var.location
-  tenant_id             = var.tenant_id
-}
-
 #############################################################################
 #                               Making a Resource Group
 #############################################################################
@@ -40,6 +31,20 @@ resource "azurerm_storage_account" "this" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
+  depends_on = [ azurerm_resource_group.this ]
+}
+
+#############################################################################
+#                               Making a kEY vAULT
+#############################################################################
+
+module "avm-res-keyvault-vault" {
+  source  = "Azure/avm-res-keyvault-vault/azurerm"
+  version = "0.5.0"
+  name = "keyvaultcgr2terraf"
+  resource_group_name   = var.resourcegroup
+  location              = var.location
+  tenant_id             = var.tenant_id
   depends_on = [ azurerm_resource_group.this ]
 }
 
